e1498fe9b495c5907e935824de87634125076018
Andre Vidal <andrinho@gmail.com>
2023-02-23 16:45:05 -0300

    imgs loaded from the public folder, but now im having issues with loading them from the actual source folder wich will go through the improvements frmo the ESlint (bundler)"

--- src/components/PicsLoader.jsx
+++ src/components/PicsLoader.jsx
@@ -1,15 +1,32 @@
 import { useEffect } from "react";
-import Img1 from "../assets/imgs/fortaleza/praiadofuturo/pf02.jpg";
+import Img1 from "../assets/imgs/fortaleza/praiadofuturo/pf01.jpg";
+import Img2 from "../assets/imgs/fortaleza/praiadofuturo/pf02.jpg";
+
+// const testAPI = "https://randomuser.me/api/";
 
 export default function PicsLoader({ urls }) {
   const { path, urls: picsUrls } = urls;
+  let imgsLoaded = false;
 
   useEffect(() => {
-    console.log(path + picsUrls[0]);
+    // img3 = fetch(path + picsUrls[0]).then((e) => e);
+    const imgs = picsUrls.map((pic) => {
+      fetch("/assets" + path + pic).then((img) => (imgsLoaded = true));
+    });
   }, []);
+
   return (
-    <div>
-      <img src={"/assets" + path + picsUrls[0]} alt=""></img>
+    <div className="pics-loader-holder">
+      {picsUrls.map((pic) => {
+        return (
+          <img
+            className="praias-pics"
+            src={"/assets" + path + pic}
+            alt="teste"
+            key={pic}
+          />
+        );
+      })}
     </div>
   );
 }
